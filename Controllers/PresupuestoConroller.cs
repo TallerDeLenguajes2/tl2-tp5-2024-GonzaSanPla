9be81649ea1dbe2f3b70801359be367c4c88e6d2
Santillán Plasencia Gonzalo <santillangon@gmail.com>
2024-10-27 18:27:04 -0300

    Listar producto funciona y agregar descipcion a medio hacer

--- Controllers/PresupuestoConroller.cs
+++ Controllers/PresupuestoConroller.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using espacioPresupuestoRepository;
 using espacioPresupuestos;
+using espacioPresupuestosDetalle;
 namespace tl2_tp5_2024_GonzaSanPla.Controllers;
 
 [ApiController]
@@ -9,13 +10,28 @@ public class PresupuestoController : ControllerBase
 {
     private PresupuestoRepository presupuestoRepositorio= new PresupuestoRepository();
     private Presupuesto presupuesto = new Presupuesto();
+    private PresupuestoDetalle detalle= new PresupuestoDetalle();
 
     [HttpPost("Cargar presupuesto")]  
-    public ActionResult cargarProduto(string nombreDestinatario,string fechaCreacion) // Hago que ingrese la fecha o uso la fecha actual? 
+    public ActionResult cargarPresupuesto(string nombreDestinatario,string fechaCreacion) // Hago que ingrese la fecha o uso la fecha actual? 
     {
         presupuesto.NombreDestinatario=nombreDestinatario;
         presupuesto.FechaCreacion=fechaCreacion;
-        presupuestoRepositorio.CrearNuevo(presupuesto);
+        presupuestoRepositorio.CrearNuevoPresupuesto(presupuesto);
         return Created();
+    }
+
+    [HttpPost("Cargar detalles presupuesto")]  
+    public ActionResult cargarPresupuestoDetalle(int idPrespuesto,int idProducto,int cantidad) // El idProducto mando como int o mando el producto? Se qchequea que el producto existe?
+    {
+        detalle.Cantidad=cantidad;
+        detalle.Producto.IdProducto=idProducto;
+        presupuestoRepositorio.CrearNuevoDetalle(idPrespuesto,detalle);
+        return Created();
+    }
+    [HttpGet("Listar Productos")]
+    public ActionResult<List<Presupuesto>> listarProducto()
+    {
+        return Ok(presupuestoRepositorio.ListarProductos());
     }
 }
--- Repositorios/PresupuestoRepository.cs
+++ Repositorios/PresupuestoRepository.cs
@@ -1,11 +1,12 @@
 using espacioPresupuestos;
+using espacioPresupuestosDetalle;
 using Microsoft.Data.Sqlite;
 
 namespace espacioPresupuestoRepository;
 public class PresupuestoRepository  
 {  
     string cadenaConexion = @"Data Source=Tienda.db;Cache=Shared";
-    public void CrearNuevo(Presupuesto pres)        // Que tipo es fecha? Porque en la BD es un string 
+    public void CrearNuevoPresupuesto(Presupuesto pres)        // Que tipo es fecha? Porque en la BD es un string 
     {
         using (SqliteConnection connection = new SqliteConnection(cadenaConexion))
         {
@@ -19,4 +20,42 @@ public class PresupuestoRepository
         }
     }
 
+    public void CrearNuevoDetalle(int idPrespuesto,PresupuestoDetalle detalles)  // El idProducto mando como int o mando el producto o lo mando dentro de detalle? Se qchequea que el producto existe?
+    {
+        using (SqliteConnection connection = new SqliteConnection(cadenaConexion))
+        {
+            var query = "INSERT INTO PresupuestosDetalle (idPrespuesto, idProducto,Cantidad ) VALUES @idPres,@idProd,@Cantidad";
+            connection.Open();
+            var command = new SqliteCommand(query, connection);
+            command.Parameters.Add(new SqliteParameter("@idPres", idPrespuesto));
+            command.Parameters.Add(new SqliteParameter("@idProd", detalles.Producto.IdProducto));
+            command.Parameters.Add(new SqliteParameter("@Cantidad", detalles.Cantidad));
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+     public List<Presupuesto> ListarProductos()
+    {
+        List<Presupuesto> listaPres= new List<Presupuesto>();
+        using (SqliteConnection connection = new SqliteConnection(cadenaConexion))
+        {
+            string query = "SELECT * FROM Presupuestos;";
+            SqliteCommand command = new SqliteCommand(query, connection);
+            connection.Open();
+            using (SqliteDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var presupuesto = new Presupuesto();
+                    presupuesto.IdPresupuesto = Convert.ToInt32(reader["idPresupuesto"]);
+                    presupuesto.NombreDestinatario = reader["NombreDestinatario"].ToString();
+                    presupuesto.FechaCreacion = reader["FechaCreacion"].ToString();
+                    listaPres.Add(presupuesto);
+                }
+            }
+            connection.Close();
+
+        }
+        return listaPres;
+    }
 }
